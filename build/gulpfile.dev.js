31426c211193d75bc1f1915a4c9412ef4156d09b
BearD01001 <dino@beard.ink>
2017-07-04 17:22:56 +0800

    npm script completed.

--- build/gulpfile.dev.js
+++ build/gulpfile.dev.js
@@ -2,16 +2,29 @@ var path       = require('path')
 var gulp       = require('gulp')
 var less       = require('gulp-less')
 var clean      = require('gulp-clean-css')
+var rename     = require('gulp-rename')
 var sourcemaps = require('gulp-sourcemaps')
 
-var distPath   = path.resolve(__dirname, '../dist')
-gulp.task('default', () => {
-console.log(111)
-    gulp.watch(path.resolve('../src/tiny-trim.less'))
+var destPath   = path.resolve(__dirname, '../dest')
+
+gulp.task('run', () => {
+    gulp.src(path.resolve('../src/tiny-trim.less'))
     .pipe(sourcemaps.init())
     .pipe(less())
-    .pipe(gulp.dist(distPath))
-    .pipe(clean())
+    .pipe(clean({ format: 'beautify' }))
+    .pipe(gulp.dest(destPath))
+    .pipe(rename('tiny-trim.min.css'))
+    .pipe(clean({
+      format: {
+        breaks: {
+          afterComment: true
+        }
+      }
+    }))
     .pipe(sourcemaps.write(path.resolve(__dirname, '../maps')))
-    .pipe(gulp.dist(distPath))
+    .pipe(gulp.dest(destPath))
+})
+
+gulp.task('default', () => {
+  gulp.watch(path.resolve('../src/*.less'), ['run'])
 })
